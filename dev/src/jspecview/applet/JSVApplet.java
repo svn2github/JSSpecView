b7eddc8ed84984e6d832abd27c0d3de224d42fb9
rjlanc <rjlanc@1262bc75-0126-4021-b206-820d13540e36>
2009-07-04 13:34:16 +0000

    minor housekeeping continued
    git-svn-id: https://svn.code.sf.net/p/jspecview/svn@191 1262bc75-0126-4021-b206-820d13540e36

--- dev/src/jspecview/applet/JSVApplet.java
+++ dev/src/jspecview/applet/JSVApplet.java
@@ -107,7 +107,7 @@ public class JSVApplet extends JApplet {
     System.out.println("JSpecView " + this + " finalized");
    }
 
- public static final String APPLET_VERSION = "1.0.20090701-1915";
+ public static final String APPLET_VERSION = "1.0.20090704-0815";
 
   /* --------------------set default-PARAMETERS -------------------------*/
   String filePath, oldfilePath;
