bf16bc391d587664b41b59a425e0e9d1f4be9025
hansonr <hansonr@1262bc75-0126-4021-b206-820d13540e36>
2012-02-07 12:53:55 +0000

    git-svn-id: https://svn.code.sf.net/p/jspecview/svn@249 1262bc75-0126-4021-b206-820d13540e36

--- dev2/JSpecView/src/org/jmol/api/JmolSyncInterface.java
+++ dev2/JSpecView/src/org/jmol/api/JmolSyncInterface.java
@@ -2,15 +2,13 @@ package org.jmol.api;
 
 
 /*
- * This interface can be used by any applet to register itself
- * with Jmol and thus allow direct applet-applet scripting and 
- * syncing operations.
+ * Used for application - application interface (Jmol-JSpecView)
  *  
  */
 public interface JmolSyncInterface {
 
   public abstract void syncScript(String script);
 
-  public abstract void registerApplet(String appletID, JmolSyncInterface applet);
+  public abstract void registerApplet(String appletID, JmolSyncInterface jsi);
 
 }
