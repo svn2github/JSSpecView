d9ce58a32a3588d6d0ddd179c0795f7ea478a516
hansonr <hansonr@1262bc75-0126-4021-b206-820d13540e36>
2012-02-11 03:49:48 +0000

    git-svn-id: https://svn.code.sf.net/p/jspecview/svn@284 1262bc75-0126-4021-b206-820d13540e36

---
+++ dev2/JSpecViewLib/src/jspecview/util/Escape.java
@@ -0,0 +1,74 @@
+/* $RCSfile$
+ * $Author$
+ * $Date$
+ * $Revision$
+ *
+ * Copyright (C) 2005  The Jmol Development Team
+ *
+ * Contact: jmol-developers@lists.sf.net
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ *  02110-1301, USA.
+ */
+
+package jspecview.util;
+
+public class Escape {
+
+  private final static String escapable = "\\\\\tt\rr\nn\"\""; 
+
+  public static String jmolSelect(String peak, String file) {
+    return "Select: "
+    + TextFormat.simpleReplace(peak, "file=\"\"", "file=\"" + escape(file)
+        + "\"");
+  }
+
+  public static String escape(String str) {
+    if (str == null)
+      return "\"\"";
+    boolean haveEscape = false;
+    int i = 0;
+    for (; i < escapable.length(); i += 2)
+      if (str.indexOf(escapable.charAt(i)) >= 0) {
+        haveEscape = true;
+        break;
+      }
+    if (haveEscape)
+      while (i < escapable.length()) {
+        int pt = -1;
+        char ch = escapable.charAt(i++);
+        char ch2 = escapable.charAt(i++);
+        StringBuffer sb = new StringBuffer();
+        int pt0 = 0;
+        while ((pt = str.indexOf(ch, pt + 1)) >= 0) {
+          sb.append(str.substring(pt0, pt)).append('\\').append(ch2);
+          pt0 = pt + 1;
+        }
+        sb.append(str.substring(pt0, str.length()));
+        str = sb.toString();
+      }
+    for (i = str.length(); --i >= 0;)
+      if (str.charAt(i) > 0x7F)
+        str = str.substring(0, i) + unicode(str.charAt(i))
+            + str.substring(i + 1);
+    return "\"" + str + "\"";
+  }
+
+  private static String unicode(char c) {
+    String s = "0000" + Integer.toHexString(c);
+    return "\\u" + s.substring(s.length() - 4);
+  }
+
+}
