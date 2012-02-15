8396df69fe6d84c24c1123018cacd61eb6e6edeb
rjlanc <rjlanc@1262bc75-0126-4021-b206-820d13540e36>
2012-02-15 00:49:53 +0000

    update time stamp to 14 Feb 18:00
    git-svn-id: https://svn.code.sf.net/p/jspecview/svn@346 1262bc75-0126-4021-b206-820d13540e36

--- dev2/JSpecView/src/jspecview/applet/JSVApplet.java
+++ dev2/JSpecView/src/jspecview/applet/JSVApplet.java
@@ -129,7 +129,7 @@ public class JSVApplet extends JApplet implements PeakPickedListener {
     System.out.println("JSpecView " + this + " finalized");
   }
 
-  public static final String APPLET_VERSION = "1.0.20120214-0500";
+  public static final String APPLET_VERSION = "1.0.20120214-1800";
 
   /* --------------------set default-PARAMETERS -------------------------*/
   String filePath, oldfilePath;
