d81471e383e651aa40eaba6f3b23b65df5557087
rjlanc <rjlanc@1262bc75-0126-4021-b206-820d13540e36>
2012-02-19 16:32:48 +0000

    added RAMAN to list of reverse spectrum display types 19th Feb 11:30
    git-svn-id: https://svn.code.sf.net/p/jspecview/svn@377 1262bc75-0126-4021-b206-820d13540e36

--- dev2/JSpecView/src/jspecview/applet/JSVApplet.java
+++ dev2/JSpecView/src/jspecview/applet/JSVApplet.java
@@ -126,7 +126,7 @@ public class JSVApplet extends JApplet implements PeakPickedListener {
     System.out.println("JSpecView " + this + " finalized");
   }
 
-  public static final String APPLET_VERSION = "1.0.20120216-2030";
+  public static final String APPLET_VERSION = "1.0.20120219-1130";
 
   /* --------------------set default-PARAMETERS -------------------------*/
   String filePath, oldfilePath;
