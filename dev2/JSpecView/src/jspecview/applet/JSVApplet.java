fcb1fa289a7ca568de674dc8761549c5d5864d3b
rjlanc <rjlanc@1262bc75-0126-4021-b206-820d13540e36>
2012-03-09 01:03:23 +0000

    update time stamp 8th March 8PM
    git-svn-id: https://svn.code.sf.net/p/jspecview/svn@670 1262bc75-0126-4021-b206-820d13540e36

--- dev2/JSpecView/src/jspecview/applet/JSVApplet.java
+++ dev2/JSpecView/src/jspecview/applet/JSVApplet.java
@@ -113,7 +113,7 @@ import netscape.javascript.JSObject;
 
 public class JSVApplet extends JApplet implements PeakPickedListener, ScriptInterface {
 
-  public static final String APPLET_VERSION = "2.0.20120307-0500"; //
+  public static final String APPLET_VERSION = "2.0.20120308-2000"; //
 //  2.0.yyyymmdd-hhmm format - should be updated to keep track of the latest version (based on Jamaica time)
   
   private static final long serialVersionUID = 1L;
