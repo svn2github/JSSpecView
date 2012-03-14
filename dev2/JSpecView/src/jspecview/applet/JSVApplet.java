15c086b25a3a53aa56880b82eedb5313df86d26a
rjlanc <rjlanc@1262bc75-0126-4021-b206-820d13540e36>
2012-03-14 09:51:59 +0000

    update time stamp 14th March 4:50AM
    git-svn-id: https://svn.code.sf.net/p/jspecview/svn@710 1262bc75-0126-4021-b206-820d13540e36

--- dev2/JSpecView/src/jspecview/applet/JSVApplet.java
+++ dev2/JSpecView/src/jspecview/applet/JSVApplet.java
@@ -112,7 +112,7 @@ import netscape.javascript.JSObject;
 
 public class JSVApplet extends JApplet implements PanelListener, ScriptInterface {
 
-  public static final String APPLET_VERSION = "2.0.20120312-0430"; //
+  public static final String APPLET_VERSION = "2.0.20120314-0450"; //
 //  2.0.yyyymmdd-hhmm format - should be updated to keep track of the latest version (based on Jamaica time)
   
   private static final long serialVersionUID = 1L;
