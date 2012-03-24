3c71e5a7d022530c26f459c3f133f48f36c5aa95
hansonr <hansonr@1262bc75-0126-4021-b206-820d13540e36>
2012-03-24 06:02:56 +0000

    git-svn-id: https://svn.code.sf.net/p/jspecview/svn@806 1262bc75-0126-4021-b206-820d13540e36

--- dev2/JSpecViewLib/src/jspecview/common/ScriptInterface.java
+++ dev2/JSpecViewLib/src/jspecview/common/ScriptInterface.java
@@ -6,31 +6,32 @@ import jspecview.source.JDXSource;
 
 
 public interface ScriptInterface {
-  public void runScript(String script);
-  public JSVPanel setSpectrum(int i);
-  public void sendFrameChange(JSVPanel jsvp);
+  
   public String execExport(JSVPanel jsvp, String value);
-  public void execOverlay(String value);
-  public void execIntegrate(String value);
-  public List<JSVSpecNode> getSpecNodes();
-  public JDXSource getCurrentSource();
-  public void execSetIntegrationRatios(String value);
-  public String setSolutionColor(boolean b);
-  public void execTAConvert(int iMode) throws Exception;
-  public void execSetCallback(ScriptToken st, String value);
-  public Parameters getParameters();
   public void execClose(String value);
-  public String execLoad(String value);
   public void execHidden(boolean b);
-  public void execSetInterface(String value);
+  public void execIntegrate(String value);
+  public String execLoad(String value);
+  public void execOverlay(String value);
   public void execScriptComplete(String msg, boolean isOK);
-  public JSVPanel execSetSpectrum(String value);
-  public JSVPanel getSelectedPanel();
   public void execSetAutoIntegrate(boolean b);
+  public void execSetCallback(ScriptToken st, String value);
+  public JSVPanel execSetSpectrum(String value);
+  public void execSetIntegrationRatios(String value);
+  public void execSetInterface(String value);
+  public void execTAConvert(int iMode) throws Exception;
   public void execTest(String value);
-  public void syncToJmol(String value);
-  public PanelData getPanelData();
 
-  //public void exportSpectrum(String actionCommand);
-  //public void printView();
+  public JDXSource getCurrentSource();
+  public PanelData getPanelData();
+  public Parameters getParameters();
+  public JSVPanel getSelectedPanel();
+  public List<JSVSpecNode> getSpecNodes();
+  
+  public void runScript(String script);
+  public void sendFrameChange(JSVPanel jsvp);
+  public JSVPanel setSpectrum(int i);
+  public String setSolutionColor(boolean b);
+  public void syncToJmol(String value);
+
 }
