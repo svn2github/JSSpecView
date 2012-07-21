16ee417e87f429df0ea8a556fc22ada1a00e7a45
hansonr <hansonr@1262bc75-0126-4021-b206-820d13540e36>
2012-07-21 14:22:14 +0000

    Set the SVN version number in TODO.txt rather than JSVapplet.java This would mean making a comment when changes were made and submitting each time so that the Help About dialog box code was updated as well.
    SET LOADIMAGINARY T/F   -- default F - we can't process the imaginary spec anyway
    acceptable?
     
    
    
    git-svn-id: https://svn.code.sf.net/p/jspecview/svn@1061 1262bc75-0126-4021-b206-820d13540e36

--- dev2/JSpecView/src/jspecview/applet/JSVApplet.java
+++ dev2/JSpecView/src/jspecview/applet/JSVApplet.java
@@ -48,7 +48,11 @@
 
 package jspecview.applet;
 
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Map;
+import java.util.Properties;
 
 import javax.swing.JApplet;
 
@@ -85,9 +89,55 @@ public class JSVApplet extends JApplet implements JSVAppletInterface {
   }
 
   private static final String SVN_REV= "$LastChangedRevision::      $";  
-  
-  public static final String APPLET_VERSION = "2.0.2012_SVN"+SVN_REV.substring(22,27); //
-//  2.0.yyyy_SVN xxxx - should be automatically updated with the latest revision number from sourceforge SVN
+  //  2.0.yyyy_SVN xxxx - should be automatically updated with the latest revision number from sourceforge SVN
+
+  public final static String version;
+  public final static String date;
+
+	static {
+		String tmpVersion = null;
+		String tmpDate = null;
+		Properties props = new Properties();
+
+		// Reading version from resource inside jar
+		BufferedInputStream bis = null;
+		InputStream is = null;
+		try {
+			is = JSVApplet.class.getClassLoader().getResourceAsStream(
+					"jspecview/common/TODO.txt");
+			bis = new BufferedInputStream(is);
+			props.load(bis);
+			tmpVersion = props.getProperty("___version", tmpVersion);
+			tmpDate = props.getProperty("___date", tmpDate);
+			if (tmpDate != null) {
+				tmpDate = tmpDate.substring(7, 23);
+				// NOTE : date is update in the properties by SVN, and is in the
+				// format
+				// $Date$"
+			}
+		} catch (IOException e) {
+			// Nothing to do
+		} finally {
+			if (bis != null) {
+				try {
+					bis.close();
+				} catch (IOException e) {
+					// Nothing to do
+				}
+			}
+			if (is != null) {
+				try {
+					is.close();
+				} catch (IOException e) {
+					// Nothing to do
+				}
+			}
+		}
+		version = (tmpVersion != null ? tmpVersion : "(Unknown version)");
+		date = (tmpDate != null ? tmpDate : "(Unknown date)");
+	}
+
+  public static final String APPLET_VERSION = version + "/SVN"+SVN_REV.substring(22,27) + "/" + date;
   
   private static final long serialVersionUID = 1L;
 
