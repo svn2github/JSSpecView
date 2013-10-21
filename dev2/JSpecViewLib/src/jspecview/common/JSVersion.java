77141a3b01142ed9fd800513f0c804b9ac2ee6f5
hansonr <hansonr@1262bc75-0126-4021-b206-820d13540e36>
2013-10-21 11:07:15 +0000

    git-svn-id: https://svn.code.sf.net/p/jspecview/svn@1357 1262bc75-0126-4021-b206-820d13540e36

--- dev2/JSpecViewLib/src/jspecview/common/JSVersion.java
+++ dev2/JSpecViewLib/src/jspecview/common/JSVersion.java
@@ -18,16 +18,14 @@
 package jspecview.common;
 
 import java.io.BufferedInputStream;
-import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
 
-import javajs.J2SRequireImport;
-
 import org.jmol.util.Txt;
 
+import javajs.J2SIgnoreImport;
 
-@J2SRequireImport({java.util.Properties.class, java.io.BufferedInputStream.class, org.jmol.util.Txt.class})
+@J2SIgnoreImport({java.util.Properties.class, java.io.InputStream.class, java.io.BufferedInputStream.class, org.jmol.util.Txt.class})
 public class JSVersion {
 
   public static final String VERSION;
@@ -37,54 +35,55 @@ public class JSVersion {
 		String tmpVersion = null;
 		String tmpDate = null;
 		String tmpSVN = null;
-		Properties props = new Properties();
 
 		// Reading version from resource inside jar
-		BufferedInputStream bis = null;
-		InputStream is = null;
-		String s = "jspecview/common/TODO.txt"; 
-		try {
-			/**
-			 * @j2sNative
-			 * 
-			 * s = "JSV/common/TODO.txt";
-			 * 
-			 */
-			{
-				
-			}
-			is = JSVersion.class.getClassLoader().getResourceAsStream(s);
-					
-			bis = new BufferedInputStream(is);
-			props.load(bis);
-			tmpVersion = Txt.trimQuotes(props.getProperty("___version", tmpVersion));
-			tmpDate = Txt.trimQuotes(props.getProperty("___date", tmpDate));
-			tmpSVN = Txt.trimQuotes(props.getProperty("___svnRev", tmpSVN));
-			if (tmpDate != null)
-				tmpDate = tmpDate.substring(7, 23);
-			if (tmpSVN != null)
-				tmpSVN = tmpSVN.substring(22,27);
-		} catch (IOException e) {
-			// Nothing to do
-		} finally {
-			if (bis != null) {
-				try {
-					bis.close();
-				} catch (IOException e) {
-					// Nothing to do
+		/**
+		 * @j2sNative
+		 * 
+		 *            tmpVersion = self.___version; tmpDate = self.___date; tmpSVN =
+		 *            self.___svnRev;
+		 * 
+		 */
+		{
+			BufferedInputStream bis = null;
+			InputStream is = null;
+			try {
+				is = JSVersion.class.getClassLoader().getResourceAsStream(
+						"jspecview/common/TODO.txt");
+				bis = new BufferedInputStream(is);
+				Properties props = new Properties();
+				props.load(bis);
+				tmpVersion = Txt
+						.trimQuotes(props.getProperty("___version", tmpVersion));
+				tmpDate = Txt.trimQuotes(props.getProperty("___date", tmpDate));
+				tmpSVN = Txt.trimQuotes(props.getProperty("___svnRev", tmpSVN));
+			} catch (Exception e) {
+				// Nothing to do
+			} finally {
+				if (bis != null) {
+					try {
+						bis.close();
+					} catch (Exception e) {
+						// Nothing to do
+					}
 				}
-			}
-			if (is != null) {
-				try {
-					is.close();
-				} catch (IOException e) {
-					// Nothing to do
+				if (is != null) {
+					try {
+						is.close();
+					} catch (Exception e) {
+						// Nothing to do
+					}
 				}
 			}
 		}
+		if (tmpDate != null)
+			tmpDate = tmpDate.substring(7, 23);
+		if (tmpSVN != null)
+			tmpSVN = tmpSVN.substring(22, 27);
 		VERSION_SHORT = (tmpVersion != null ? tmpVersion : "(Unknown version)");
-		VERSION = VERSION_SHORT + "/SVN" + tmpSVN + "/" + (tmpDate != null ? tmpDate : "(Unknown date)");
-  }
+		VERSION = VERSION_SHORT + "/SVN" + tmpSVN + "/"
+				+ (tmpDate != null ? tmpDate : "(Unknown date)");
+	}
 
   
 }
