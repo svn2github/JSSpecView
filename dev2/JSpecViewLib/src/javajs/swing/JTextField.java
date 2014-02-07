6931bcf7e252471713376cd2acba29b44452ea6e
hansonr <hansonr@1262bc75-0126-4021-b206-820d13540e36>
2014-02-07 04:27:57 +0000

    git-svn-id: https://svn.code.sf.net/p/jspecview/svn@1527 1262bc75-0126-4021-b206-820d13540e36

--- dev2/JSpecViewLib/src/javajs/swing/JTextField.java
+++ dev2/JSpecViewLib/src/javajs/swing/JTextField.java
@@ -3,14 +3,6 @@ package javajs.swing;
 
 import javajs.util.SB;
 
-/**
- * Note that in javajs.swing, JTextField extends AbstractButton
- * rather than JComponent. This is to reduce the duplication of 
- * actionListener-related business.
- * 
- * @author hansonr
- *
- */
 public class JTextField extends JComponent {
 
 	public JTextField(String value) {
@@ -24,5 +16,6 @@ public class JTextField extends JComponent {
 		sb.append("<input type=text id='" + id + "' class='JTextField' style='" + getCSSstyle(0) + "' value='"+ text + "' onkeyup	=SwingController.click(this,event)	>");
 		return sb.toString();
 	}
+
 
 }
